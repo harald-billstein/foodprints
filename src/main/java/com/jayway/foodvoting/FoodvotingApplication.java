2a6f8d03e901326aa5c57019c6f736c402f6da8d
Henrik Järnbrand <henrik.jarnbrand@jayway.com>
2018-09-14 11:02:53 +0200

    Add Spring boot

---
+++ src/main/java/com/jayway/foodvoting/FoodvotingApplication.java
@@ -0,0 +1,12 @@
+package com.jayway.foodvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodvotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodvotingApplication.class, args);
+	}
+}
