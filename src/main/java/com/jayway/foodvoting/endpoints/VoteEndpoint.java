cec5eda39d5aec33e9473582a68f29ede9cce7a6
Harald Billstein <harald.billstein@gmail.com>
2018-09-18 11:28:43 +0200

    Voteendpoint added

---
+++ src/main/java/com/jayway/foodvoting/endpoints/VoteEndpoint.java
@@ -0,0 +1,30 @@
+package com.jayway.foodvoting.endpoints;
+
+import com.jayway.foodvoting.VoteHandler;
+import com.jayway.foodvoting.model.VoteResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1")
+public class VoteEndpoint {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(VoteEndpoint.class);
+
+  private VoteHandler voteHandler;
+
+  public VoteEndpoint(VoteHandler voteHandler) {
+    this.voteHandler = voteHandler;
+  }
+
+  @PostMapping(value = "/votes/")
+  public ResponseEntity<VoteResponse> vote(@RequestParam String vote) {
+    LOGGER.info("ENDPOINT TRIGGERED : " + vote);
+    return voteHandler.registerVote(vote);
+  }
+}
