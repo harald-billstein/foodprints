05ee2e806c628b95547e8dfdf0a9e67c4d83eff6
Harald Billstein <harald.billstein@gmail.com>
2018-09-20 00:08:47 +0200

    Added method to run when spring is booted (for testing)

--- src/main/java/com/jayway/foodvoting/FoodvotingApplication.java
+++ src/main/java/com/jayway/foodvoting/FoodvotingApplication.java
@@ -1,12 +1,26 @@
 package com.jayway.foodvoting;
 
+import com.jayway.foodvoting.service.Restaurants;
+import com.jayway.foodvoting.service.RestaurantsSuggestion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class FoodvotingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodvotingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FoodvotingApplication.class, args);
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void doSomethingAfterStartup() {
+    RestaurantsSuggestion restaurantsSuggestion = new RestaurantsSuggestion();
+    Restaurants restaurants = restaurantsSuggestion.getRestaurants();
+
+    System.out.println(restaurants.getRegion().getCenter().getLatitude());
+
+
+  }
 }
