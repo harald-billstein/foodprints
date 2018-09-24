f885b36da04e7a00e67feaf3f66fe1c6b93999dc
Harald Billstein <harald.billstein@gmail.com>
2018-09-24 10:58:31 +0200

    Removed testing code

--- src/main/java/com/jayway/foodvoting/FoodvotingApplication.java
+++ src/main/java/com/jayway/foodvoting/FoodvotingApplication.java
@@ -1,13 +1,13 @@
 package com.jayway.foodvoting;
 
-import com.jayway.foodvoting.service.Restaurants;
-import com.jayway.foodvoting.service.RestaurantsSuggestion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FoodvotingApplication {
 
   public static void main(String[] args) {
@@ -16,11 +16,5 @@ public class FoodvotingApplication {
 
   @EventListener(ApplicationReadyEvent.class)
   public void doSomethingAfterStartup() {
-    RestaurantsSuggestion restaurantsSuggestion = new RestaurantsSuggestion();
-    Restaurants restaurants = restaurantsSuggestion.getRestaurants();
-
-    System.out.println(restaurants.getRegion().getCenter().getLatitude());
-
-
   }
 }
