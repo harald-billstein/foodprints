371671bda3ba9103244ef91aa81906ac6ea70dfd
Henrik Järnbrand <henrik.jarnbrand@jayway.com>
2018-09-25 15:53:37 +0200

    Emission statistics

--- src/test/java/com/jayway/foodvoting/controller/FoodvotingApplicationTests.java
+++ src/test/java/com/jayway/foodvoting/controller/FoodvotingApplicationTests.java
@@ -1,18 +1,15 @@
-package com.jayway.foodvoting;
+package com.jayway.foodvoting.integrationtest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.jayway.foodvoting.integrationtest.IntegrationTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @ActiveProfiles(profiles = {"dev"})
 @RunWith(SpringRunner.class)
