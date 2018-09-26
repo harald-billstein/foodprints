2b54d8230a04345d370378d8e2f7c451006836b7
Henrik Järnbrand <henrik.jarnbrand@jayway.com>
2018-09-26 08:46:41 +0200

    Create beef for testing in Bootstrap

--- src/main/java/com/jayway/foodvoting/bootstrap/DevBootstrap.java
+++ src/main/java/com/jayway/foodvoting/bootstrap/DevBootstrap.java
@@ -1,9 +1,14 @@
 package com.jayway.foodvoting.bootstrap;
 
+import com.jayway.foodvoting.dao.Emission;
 import com.jayway.foodvoting.enums.FoodPicks;
 import com.jayway.foodvoting.model.CollectionOfVotes;
+import com.jayway.foodvoting.repository.FoodTypeRepository;
+import com.jayway.foodvoting.repository.VoteingRepository;
 import com.jayway.foodvoting.repository.VotingRepositoryIMP;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
@@ -15,9 +20,13 @@ import org.springframework.stereotype.Component;
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
   private VotingRepositoryIMP votingRepositoryIMP;
+  private VoteingRepository voteingRepository;
+  private FoodTypeRepository foodTypeRepository;
 
-  public DevBootstrap(VotingRepositoryIMP votingRepositoryIMP) {
+  public DevBootstrap(VotingRepositoryIMP votingRepositoryIMP, VoteingRepository voteingRepository, FoodTypeRepository foodTypeRepository) {
     this.votingRepositoryIMP = votingRepositoryIMP;
+    this.voteingRepository = voteingRepository;
+    this.foodTypeRepository = foodTypeRepository;
   }
 
   @Override
@@ -26,6 +35,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
   }
 
   private void init() {
+    initBeef();
+
+    Map<String, Emission> foodTypes = new HashMap<>();
+    for (Emission e: foodTypeRepository.findAll()){
+      foodTypes.put(e.getCategory().name(), e);
+    }
 
     var history = 5;
 
@@ -38,34 +53,50 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     for (int i = 0; i < history; i++) {
       collectionOfMeatVotes = new CollectionOfVotes();
       collectionOfMeatVotes.setLocalDate(LocalDate.now().minusDays(i));
-      collectionOfMeatVotes.setFoodPick(FoodPicks.MEAT);
+      collectionOfMeatVotes.setCategory(foodTypes.get("BEEF"));
       // RANDOM INT 1-10
       collectionOfMeatVotes.setVotes(new Random().nextInt(((10 - 2) + 1) + 2));
       votingRepositoryIMP.saveCollectionOfVotes(collectionOfMeatVotes);
 
       collectionOfChickenVotes = new CollectionOfVotes();
       collectionOfChickenVotes.setLocalDate(LocalDate.now().minusDays(i));
-      collectionOfChickenVotes.setFoodPick(FoodPicks.CHICKEN);
+      collectionOfChickenVotes.setCategory(foodTypes.get("CHICKEN"));
       collectionOfChickenVotes.setVotes(new Random().nextInt(((10 - 2) + 1) + 2));
       votingRepositoryIMP.saveCollectionOfVotes(collectionOfChickenVotes);
 
       collectionOfFishVotes = new CollectionOfVotes();
       collectionOfFishVotes.setLocalDate(LocalDate.now().minusDays(i));
-      collectionOfFishVotes.setFoodPick(FoodPicks.FISH);
+      collectionOfFishVotes.setCategory(foodTypes.get("FISH"));
       collectionOfFishVotes.setVotes(new Random().nextInt(((10 - 2) + 1) + 2));
       votingRepositoryIMP.saveCollectionOfVotes(collectionOfFishVotes);
 
       collectionOfVegetarianVotes = new CollectionOfVotes();
       collectionOfVegetarianVotes.setLocalDate(LocalDate.now().minusDays(i));
-      collectionOfVegetarianVotes.setFoodPick(FoodPicks.VEGETARIAN);
+      collectionOfVegetarianVotes.setCategory(foodTypes.get("VEGETARIAN"));
       collectionOfVegetarianVotes.setVotes(new Random().nextInt(((10 - 2) + 1) + 2));
       votingRepositoryIMP.saveCollectionOfVotes(collectionOfVegetarianVotes);
 
       collectionOfVeganVotes = new CollectionOfVotes();
       collectionOfVeganVotes.setLocalDate(LocalDate.now().minusDays(i));
-      collectionOfVeganVotes.setFoodPick(FoodPicks.VEGAN);
+      collectionOfVeganVotes.setCategory(foodTypes.get("VEGAN"));
       collectionOfVeganVotes.setVotes(new Random().nextInt(((10 - 2) + 1) + 2));
       votingRepositoryIMP.saveCollectionOfVotes(collectionOfVeganVotes);
     }
+  }
+
+  private void initBeef(){
+    Emission beef = foodTypeRepository.findByCategory("BEEF");
+
+    //Week 36
+    voteingRepository.saveAndFlush(new CollectionOfVotes(LocalDate.parse("2018-09-03"), beef, 1));
+    voteingRepository.saveAndFlush(new CollectionOfVotes(LocalDate.parse("2018-09-04"), beef, 2));
+    voteingRepository.saveAndFlush(new CollectionOfVotes(LocalDate.parse("2018-09-05"), beef, 1));
+    voteingRepository.saveAndFlush(new CollectionOfVotes(LocalDate.parse("2018-09-06"), beef, 3));
+    voteingRepository.saveAndFlush(new CollectionOfVotes(LocalDate.parse("2018-09-07"), beef, 1));
+
+    // Week 37
+    voteingRepository.saveAndFlush(new CollectionOfVotes(LocalDate.parse("2018-09-10"), beef, 4));
+    voteingRepository.saveAndFlush(new CollectionOfVotes(LocalDate.parse("2018-09-11"), beef, 1));
+    voteingRepository.saveAndFlush(new CollectionOfVotes(LocalDate.parse("2018-09-12"), beef, 5));
   }
 }
--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -13,18 +13,7 @@ INSERT INTO emission (category, co2e) VALUES ('BEEF', 26.0);
 INSERT INTO emission (category, co2e) VALUES ('PORK', 3.0);
 INSERT INTO emission (category, co2e) VALUES ('CHICKEN', 6.0);
 INSERT INTO emission (category, co2e) VALUES ('VEGAN', 1.1);
-INSERT INTO emission (category, co2e) VALUES ('VEGITARIAN', 1.9);
+INSERT INTO emission (category, co2e) VALUES ('VEGETARIAN', 1.9);
 INSERT INTO emission (category, co2e) VALUES ('FISH', 3.0);
 
--- CREATE BEEF ENTRIES
--- Week 36
-INSERT INTO collection_of_votes (id, local_date, votes, category) VALUES (1, '2018-09-03', 1, 'BEEF');
-INSERT INTO collection_of_votes (id, local_date, votes, category) VALUES (2, '2018-09-04', 2, 'BEEF');
-INSERT INTO collection_of_votes (id, local_date, votes, category) VALUES (3, '2018-09-05', 1, 'BEEF');
-INSERT INTO collection_of_votes (id, local_date, votes, category) VALUES (4, '2018-09-06', 3, 'BEEF');
-INSERT INTO collection_of_votes (id, local_date, votes, category) VALUES (5, '2018-09-07', 1, 'BEEF');
 
--- Week 37
-INSERT INTO collection_of_votes (id, local_date, votes, category) VALUES (6, '2018-09-10', 4, 'BEEF');
-INSERT INTO collection_of_votes (id, local_date, votes, category) VALUES (7, '2018-09-11', 1, 'BEEF');
-INSERT INTO collection_of_votes (id, local_date, votes, category) VALUES (8, '2018-09-12', 5, 'BEEF');
