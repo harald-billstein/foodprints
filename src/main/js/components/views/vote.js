f36486a5e91cc1c5c5c37507f1d2f81184c648d3
Gabriella Björk <gabriellabjork@gabriellas-mbp.lan>
2018-09-27 15:36:48 +0200

    added routes and basic stylesheets

---
+++ src/main/js/components/views/vote.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default class Vote extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {foods: []};
+        this.url = "https://localhost:8443/v1/categories/";
+    }
+
+    postVote(e) {
+        e.preventDefault();
+        // fetch me post ba till den andra
+        // måste göra om listan till en form så man kan skicka input härifrån
+    }
+
+    componentDidMount() {
+        fetch(this.url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({foods: data})})
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        return (
+            <div className="voteList">
+                <h1> Unknown. </h1>
+                <ul id="voteList">
+                    {this.state.foods.map(food => (
+                    <li> {food}. </li>))}
+                </ul>
+            </div>
+        )
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,45 @@
 var path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/main/js/app.js',
+    entry: './src/main/js/index.js',
     devtool: 'sourcemaps',
     cache: true,
-    debug: true,
     output: {
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
-    module: {
+      module: {
         loaders: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                }
+          {
+            test: /\.js|.jsx?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader',
+            query: {
+              presets: ['react', 'es2015']
             }
+          },
+          {
+            test: /\.s?css$/,  // scss & css files
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+                ]
+          },
+          {
+            test: /\.(gif|svg|jpg|png)$/,
+            loader: "file-loader",
+          },
+          {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+          },
+          {
+            test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+            loader: 'url-loader?limit=100000',
+          }
         ]
-    }
+      }
 };
