0dbbfbf6c859541c2f3c9d941ea5590c394fea4a
Harald Billstein <harald.billstein@gmail.com>
2018-09-27 23:00:35 +0200

    new test case added

--- src/test/java/com/jayway/foodvoting/integrationtest/RestaurantSuggestionsEndpointTest.java
+++ src/test/java/com/jayway/foodvoting/integrationtest/RestaurantSuggestionsEndpointTest.java
@@ -23,27 +23,36 @@ public class RestaurantSuggestionsEndpointTest extends IntegrationTest {
   private static final int MIN_RATING = 3;
   private static final int MIN_NAME = 2;
   private static final int MIN_ADDRESS_LENGTH = 5;
+  private static final long NO_CONTENT_RESPONSE = 204;
+  private static final long BAD_PATH_RESPONSE = 404;
 
   private static final String PATH = "https://localhost:8443/v1/restaurants/suggestion";
+  private static final String BAD_PATH = "https://localhost:8443/bad/path";
 
   @MockBean
   private YelpRestaurantFetcher yelpRestaurantFetcher;
   private String jsonBusinesses = RestaurantsJsonWrapper.getRestaurantSuggestionsEndpointJSON();
 
-  public void setUpMock() {
-    ObjectMapper objectMapper = new ObjectMapper();
+  public void setUpMock(boolean brokenLink) {
 
-    try {
+    if (!brokenLink) {
+      ObjectMapper objectMapper = new ObjectMapper();
+      Restaurants restaurants = new Restaurants();
+      try {
+        Mockito.when(yelpRestaurantFetcher.getRestaurants())
+            .thenReturn(objectMapper.readValue(jsonBusinesses, restaurants.getClass()));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } else {
       Mockito.when(yelpRestaurantFetcher.getRestaurants())
-          .thenReturn(objectMapper.readValue(jsonBusinesses, Restaurants.class));
-    } catch (IOException e) {
-      e.printStackTrace();
+          .thenReturn(null);
     }
   }
 
   @Test
   public void fetchRestaurantsTest() throws Exception {
-    setUpMock();
+    setUpMock(false);
     ObjectMapper objectMapper = new ObjectMapper();
 
     RestaurantSuggestionResponse firstResultObject;
@@ -71,5 +80,21 @@ public class RestaurantSuggestionsEndpointTest extends IntegrationTest {
     Assert.assertTrue(firstResultObject.getGrade() > MIN_RATING);
 
     Assert.assertNotEquals(firstResultObject.getName(), secondResultObject.getName());
+  }
+
+  @Test
+  public void fetchRestaurantsNoSuggestionsTest() throws Exception {
+    setUpMock(true);
+
+    ResultActions result = this.mvcPerformValidGet(PATH);
+    long response = result.andReturn().getResponse().getStatus();
+    Assert.assertEquals(NO_CONTENT_RESPONSE, response);
+  }
+
+  @Test
+  public void fetchRestaurantsBadUrlTest() throws Exception {
+    ResultActions result = this.mvcPerformValidGet(BAD_PATH);
+    long response = result.andReturn().getResponse().getStatus();
+    Assert.assertEquals(BAD_PATH_RESPONSE, response);
   }
 }
