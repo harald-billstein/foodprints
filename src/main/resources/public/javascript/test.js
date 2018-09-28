1a754dea2bbfc7d07203b727bd05194b946ac800
Harald Billstein <harald.billstein@gmail.com>
2018-09-28 17:59:09 +0200

    removed unused files

--- src/main/resources/public/javascript/test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $.get("https://localhost:8443/v1/restaurants/suggestion",
-      function (data, status) {
-        console.log("Restaurant name: " + data.name);
-        console.log("Restaurant address: " + data.address);
-        console.log("Restaurant grade: " + data.grade);
-      });
-});
