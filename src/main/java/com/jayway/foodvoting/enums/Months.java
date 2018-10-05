38a23fad0b640693119c07123f069bdff15adae1
Harald Billstein <harald.billstein@gmail.com>
2018-10-05 15:01:42 +0200

    unused enums added (for later use)

---
+++ src/main/java/com/jayway/foodvoting/enums/Months.java
@@ -0,0 +1,28 @@
+package com.jayway.foodvoting.enums;
+
+public enum Months {
+  JANUARY (0),
+  FEBRUARY(1),
+  MARCH(2),
+  APRIL(3),
+  MAY(4),
+  JUNE(5),
+  JULY(6),
+  AUGUST(7),
+  SEPTEMBER(8),
+  OCTOBER(9),
+  NOVEMBER(10),
+  DECEMBER(11);
+
+  private final int value;
+
+  private Months(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+
+}
